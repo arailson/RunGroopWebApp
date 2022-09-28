77bf5fda1e1af1ecd3c1949ef60d3730ba584dd6
Arailson <arailson.vieira@gmail.com>
2022-09-28 13:47:39 -0300

    Views

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly AplicationDbContext _context;
+
+        public RaceController(AplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
