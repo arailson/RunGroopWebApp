f692b20c09c66bc6b227b2c1af142aa7a578b4e7
Arailson <arailson.vieira@gmail.com>
2022-10-06 16:41:43 -0300

    Dependency Injection and Repository Patern

---
+++ RunGroopWebApp/Repository/ClubRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly AplicationDbContext _context;
+        public ClubRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public bool Add(Club club)
+        {
+            _context.Clubs.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            _context.Clubs.Remove(club);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsync(int id)
+        {
+            return await _context.Clubs.Include(i => i.Address).FirstOrDefaultAsync(club => club.Id == id);
+        }
+
+        public async Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            return await _context.Clubs.Where(club => club.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Club club)
+        {
+            _context.Clubs.Update(club);
+            return Save();
+        }
+    }
+}
